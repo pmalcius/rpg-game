9bad484a2ffdcecc40676445a0e256bb0017b1b4
pmalcius <pauliusmalcius@gmail.com>
2024-08-04 17:40:23 -0500

    Made the enemy flicker when taking damage

--- TopDownRPG/Assets/SwordAttack.cs
+++ TopDownRPG/Assets/SwordAttack.cs
@@ -22,57 +22,26 @@ public class SwordAttack : MonoBehaviour
             // up attack
             transform.localPosition = new Vector3(rightAttackOffset.y * -(1/2), rightAttackOffset.x * -(1/2));
             transform.Rotate(0, 0, 90);
-            swordCollider.enabled = true;
-
-            // Disable the collider after a short delay
-            //StartCoroutine(DisableColliderAfterDelay());
-
-            // Rotates back to (0, 0, 0)
-            //transform.Rotate(0, 0, 270);            
+            swordCollider.enabled = true;            
         } else if (attackDirectionY < 0) {
             // down attack
             transform.localPosition = new Vector3(rightAttackOffset.y * -(3/4), rightAttackOffset.x * -(2));
             transform.Rotate(0, 0, 90);
             swordCollider.enabled = true;
-
-            // Disable the collider after a short delay
-            //StartCoroutine(DisableColliderAfterDelay());
-
-            // Rotates back to (0, 0, 0)
-            //transform.Rotate(0, 0, 270);
         } else if (attackDirectionY == 0) {
             // right or left attack
             if (attackDirectionX > 0) {
                 //right attack
                 transform.localPosition = rightAttackOffset;
                 swordCollider.enabled = true;
-
-                // Disable the collider after a short delay
-                //StartCoroutine(DisableColliderAfterDelay());
             } else {
                 //left attack
                 transform.localPosition = new Vector3(rightAttackOffset.x * -1, rightAttackOffset.y);
                 swordCollider.enabled = true;
-
-                // Disable the collider after a short delay
-                //StartCoroutine(DisableColliderAfterDelay());
             }
         }
     }
 
-    // Make a startAttack
-
-
-    // Instead of the timmer, I will use a endAttack Animation after the attack is done.
-
-
-    private IEnumerator DisableColliderAfterDelay() {
-        yield return new WaitForSeconds(0.1f); // Adjust delay to match attack animation
-        swordCollider.enabled = false;
-        //Debug.Log("Sword hitbox deactivated");
-    }
-
-    // Unneeded as well since DisableColliderAfterDelay disables the sword
     public void StopAttack() {
         swordCollider.enabled = false;
         transform.rotation = startQuaternion;
@@ -87,7 +56,18 @@ public class SwordAttack : MonoBehaviour
             if (enemy != null) {
                 print("Health: " + enemy.Health);
                 enemy.Health -= damage;
+                SpriteRenderer enemySpriteRenderer = other.GetComponent<SpriteRenderer>();
+                if (enemySpriteRenderer != null) {
+                    StartCoroutine(FlickerSprite(enemySpriteRenderer));
+                }
             }
         }
+    }
+
+    private IEnumerator FlickerSprite(SpriteRenderer enemySpriteRenderer) {
+        Color originalColor = enemySpriteRenderer.color;
+        enemySpriteRenderer.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        enemySpriteRenderer.color = originalColor;
     }
 }
